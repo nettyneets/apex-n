42aef57cf74bf78899cdc3cf43ff737ddb065a54
nettyneets <nettyneets@hotmail.com>
2019-07-08 02:28:41 +0100

    Update apex-n-css.js
    gunbreaker and dancer jobs added

--- js/apex-n/apex-n-css.js
+++ js/apex-n/apex-n-css.js
@@ -13,22 +13,22 @@ if (classColors == true && dpsGradient == true) {
   document.getElementById('classColors').innerHTML =
     "#overlay .bar { background: linear-gradient(to right, " + classOne + "," + classTwo + " );}\
   #overlay .dps { background: linear-gradient(to right, " + classOne + "," + classTwo + " );" + gradientText + ";}\
-  .job-Pld  .bar, .job-War  .bar, .job-Drk  .bar, .job-Gld  .bar, .job-Mrd  .bar {" + gradientTank + "!important;}\
+  .job-Pld  .bar, .job-War  .bar, .job-Drk  .bar, .job-Gld  .bar, .job-Mrd  .bar, .job-Gnb  .bar {" + gradientTank + "!important;}\
   .job-Sch  .bar, .job-Ast  .bar, .job-Whm  .bar, .job-Cnj  .bar {" + gradientHealer + "!important;}\
   .job-Drg  .bar, .job-Mnk  .bar, .job-Nin  .bar, .job-Blm  .bar, .job-Brd  .bar, .job-Smn  .bar,\
-  .job-Rog  .bar, .job-Thm  .bar, .job-Arc  .bar, .job-Acn  .bar, .job-Pgl  .bar, .job-Lnc  .bar, .job-Mch  .bar, .job-Rdm  .bar, .job-Sam  .bar {" + gradientDps + "!important;}\
-  .job-Pld  .dps, .job-War  .dps, .job-Drk  .dps, .job-Gld  .dps, .job-Mrd  .dps {" + gradientTank + "!important; " + gradientText + ";}\
+  .job-Rog  .bar, .job-Thm  .bar, .job-Arc  .bar, .job-Acn  .bar, .job-Pgl  .bar, .job-Lnc  .bar, .job-Mch  .bar, .job-Rdm  .bar, .job-Sam  .bar, .job-Dnc  .bar {" + gradientDps + "!important;}\
+  .job-Pld  .dps, .job-War  .dps, .job-Drk  .dps, .job-Gld  .dps, .job-Mrd  .dps, .job-Gnb  .dps {" + gradientTank + "!important; " + gradientText + ";}\
   .job-Sch  .dps, .job-Ast  .dps, .job-Whm  .dps, .job-Cnj  .dps {" + gradientHealer + "!important; " + gradientText + ";}\
   .job-Drg  .dps, .job-Mnk  .dps, .job-Nin  .dps, .job-Blm  .dps, .job-Brd  .dps, .job-Smn  .dps,\
-  .job-Rog  .dps, .job-Thm  .dps, .job-Arc  .dps, .job-Acn  .dps, .job-Pgl  .dps, .job-Lnc  .dps, .job-Mch  .dps, .job-Rdm  .dps, .job-Sam  .dps {" + gradientDps + "!important; " + gradientText + ";}\
-  .job-Pld  .statone, .job-War  .statone, .job-Drk  .statone, .job-Gld  .statone, .job-Mrd  .statone { color: " + classTankOne + "!important;}\
-  .job-Pld  .stattwo, .job-War  .stattwo, .job-Drk  .stattwo, .job-Gld  .stattwo, .job-Mrd  .stattwo { color: " + classTankTwo + "!important;}\
+  .job-Rog  .dps, .job-Thm  .dps, .job-Arc  .dps, .job-Acn  .dps, .job-Pgl  .dps, .job-Lnc  .dps, .job-Mch  .dps, .job-Rdm  .dps, .job-Sam  .dps, .job-Dnc  .dps {" + gradientDps + "!important; " + gradientText + ";}\
+  .job-Pld  .statone, .job-War  .statone, .job-Drk  .statone, .job-Gld  .statone, .job-Mrd  .statone, .job-Gnb  .statone { color: " + classTankOne + "!important;}\
+  .job-Pld  .stattwo, .job-War  .stattwo, .job-Drk  .stattwo, .job-Gld  .stattwo, .job-Mrd  .stattwo, .job-Gnb  .stattwo { color: " + classTankTwo + "!important;}\
   .job-Sch  .statone, .job-Ast  .statone, .job-Whm  .statone, .job-Cnj  .statone { color: " + classHealerOne + "!important;}\
   .job-Sch  .stattwo, .job-Ast  .stattwo, .job-Whm  .stattwo, .job-Cnj  .stattwo { color: " + classHealerTwo + "!important;}\
   .job-Drg  .statone, .job-Mnk  .statone, .job-Nin  .statone, .job-Blm  .statone, .job-Brd  .statone, .job-Smn  .statone,\
-  .job-Rog  .statone, .job-Thm  .statone, .job-Arc  .statone, .job-Acn  .statone, .job-Pgl  .statone, .job-Lnc  .statone, .job-Mch  .statone, .job-Rdm  .statone, .job-Sam  .statone { color: " + classDpsOne + "!important;}\
+  .job-Rog  .statone, .job-Thm  .statone, .job-Arc  .statone, .job-Acn  .statone, .job-Pgl  .statone, .job-Lnc  .statone, .job-Mch  .statone, .job-Rdm  .statone, .job-Sam  .statone, .job-Dnc  .statone { color: " + classDpsOne + "!important;}\
   .job-Drg  .stattwo, .job-Mnk  .stattwo, .job-Nin  .stattwo, .job-Blm  .stattwo, .job-Brd  .stattwo, .job-Smn  .stattwo,\
-  .job-Rog  .stattwo, .job-Thm  .stattwo, .job-Arc  .stattwo, .job-Acn  .stattwo, .job-Pgl  .stattwo, .job-Lnc  .stattwo, .job-Mch  .stattwo, .job-Rdm  .stattwo, .job-Sam  .stattwo { color: " + classDpsTwo + "!important;}\
+  .job-Rog  .stattwo, .job-Thm  .stattwo, .job-Arc  .stattwo, .job-Acn  .stattwo, .job-Pgl  .stattwo, .job-Lnc  .stattwo, .job-Mch  .stattwo, .job-Rdm  .stattwo, .job-Sam  .stattwo, .job-Dnc  .stattwo { color: " + classDpsTwo + "!important;}\
   .statone { color: " + classOne + "!important;} .stattwo { color: " + classTwo + "!important;}\
   .encountertime { color: " + classColorOne + "!important;} .maxhit { color: " + classColorTwo + "!important;}";
 } if (classColors == true && dpsGradient == false) {
@@ -38,22 +38,22 @@ if (classColors == true && dpsGradient == true) {
   document.getElementById('classColors').innerHTML =
     "#overlay .bar { background: linear-gradient(to right, " + classOne + "," + classTwo + " );}\
   #overlay .dps { color: " + classDps + "; background: none!important; -webkit-text-fill-color: initial!important;}\
-  .job-Pld  .bar, .job-War  .bar, .job-Drk  .bar, .job-Gld  .bar, .job-Mrd  .bar {" + gradientTank + "!important;}\
+  .job-Pld  .bar, .job-War  .bar, .job-Drk  .bar, .job-Gld  .bar, .job-Mrd  .bar, .job-Gnb  .bar {" + gradientTank + "!important;}\
   .job-Sch  .bar, .job-Ast  .bar, .job-Whm  .bar, .job-Cnj  .bar {" + gradientHealer + "!important;}\
   .job-Drg  .bar, .job-Mnk  .bar, .job-Nin  .bar, .job-Blm  .bar, .job-Brd  .bar, .job-Smn  .bar,\
-  .job-Rog  .bar, .job-Thm  .bar, .job-Arc  .bar, .job-Acn  .bar, .job-Pgl  .bar, .job-Lnc  .bar, .job-Mch  .bar, .job-Rdm  .bar, .job-Sam  .bar {" + gradientDps + "!important;}\
-  .job-Pld  .dps, .job-War  .dps, .job-Drk  .dps, .job-Gld  .dps, .job-Mrd  .dps { color: " + classTankDps + "!important;}\
+  .job-Rog  .bar, .job-Thm  .bar, .job-Arc  .bar, .job-Acn  .bar, .job-Pgl  .bar, .job-Lnc  .bar, .job-Mch  .bar, .job-Rdm  .bar, .job-Sam  .bar, .job-Dnc  .bar {" + gradientDps + "!important;}\
+  .job-Pld  .dps, .job-War  .dps, .job-Drk  .dps, .job-Gld  .dps, .job-Mrd  .dps, .job-Gnb  .dps { color: " + classTankDps + "!important;}\
   .job-Sch  .dps, .job-Ast  .dps, .job-Whm  .dps, .job-Cnj  .dps { color: " + classHealerDps + "!important;}\
   .job-Drg  .dps, .job-Mnk  .dps, .job-Nin  .dps, .job-Blm  .dps, .job-Brd  .dps, .job-Smn  .dps,\
-  .job-Rog  .dps, .job-Thm  .dps, .job-Arc  .dps, .job-Acn  .dps, .job-Pgl  .dps, .job-Lnc  .dps, .job-Mch  .dps, .job-Rdm  .dps, .job-Sam  .dps { color: " + classDpsDps + "!important;}\
-  .job-Pld  .statone, .job-War  .statone, .job-Drk  .statone, .job-Gld  .statone, .job-Mrd  .statone { color: " + classTankOne + "!important;}\
-  .job-Pld  .stattwo, .job-War  .stattwo, .job-Drk  .stattwo, .job-Gld  .stattwo, .job-Mrd  .stattwo { color: " + classTankTwo + "!important;}\
+  .job-Rog  .dps, .job-Thm  .dps, .job-Arc  .dps, .job-Acn  .dps, .job-Pgl  .dps, .job-Lnc  .dps, .job-Mch  .dps, .job-Rdm  .dps, .job-Sam  .dps, .job-Dnc  .dps { color: " + classDpsDps + "!important;}\
+  .job-Pld  .statone, .job-War  .statone, .job-Drk  .statone, .job-Gld  .statone, .job-Mrd  .statone, .job-Gnb  .statone { color: " + classTankOne + "!important;}\
+  .job-Pld  .stattwo, .job-War  .stattwo, .job-Drk  .stattwo, .job-Gld  .stattwo, .job-Mrd  .stattwo, .job-Gnb  .stattwo { color: " + classTankTwo + "!important;}\
   .job-Sch  .statone, .job-Ast  .statone, .job-Whm  .statone, .job-Cnj  .statone { color: " + classHealerOne + "!important;}\
   .job-Sch  .stattwo, .job-Ast  .stattwo, .job-Whm  .stattwo, .job-Cnj  .stattwo { color: " + classHealerTwo + "!important;}\
   .job-Drg  .statone, .job-Mnk  .statone, .job-Nin  .statone, .job-Blm  .statone, .job-Brd  .statone, .job-Smn  .statone,\
-  .job-Rog  .statone, .job-Thm  .statone, .job-Arc  .statone, .job-Acn  .statone, .job-Pgl  .statone, .job-Lnc  .statone, .job-Mch  .statone, .job-Rdm  .statone, .job-Sam  .statone { color: " + classDpsOne + "!important;}\
+  .job-Rog  .statone, .job-Thm  .statone, .job-Arc  .statone, .job-Acn  .statone, .job-Pgl  .statone, .job-Lnc  .statone, .job-Mch  .statone, .job-Rdm  .statone, .job-Sam  .statone, .job-Dnc  .statone { color: " + classDpsOne + "!important;}\
   .job-Drg  .stattwo, .job-Mnk  .stattwo, .job-Nin  .stattwo, .job-Blm  .stattwo, .job-Brd  .stattwo, .job-Smn  .stattwo,\
-  .job-Rog  .stattwo, .job-Thm  .stattwo, .job-Arc  .stattwo, .job-Acn  .stattwo, .job-Pgl  .stattwo, .job-Lnc  .stattwo, .job-Mch  .stattwo, .job-Rdm  .stattwo, .job-Sam  .stattwo { color: " + classDpsTwo + "!important;}\
+  .job-Rog  .stattwo, .job-Thm  .stattwo, .job-Arc  .stattwo, .job-Acn  .stattwo, .job-Pgl  .stattwo, .job-Lnc  .stattwo, .job-Mch  .stattwo, .job-Rdm  .stattwo, .job-Sam  .stattwo, .job-Dnc  .stattwo { color: " + classDpsTwo + "!important;}\
   .statone { color: " + classOne + "!important;} .stattwo { color: " + classTwo + "!important;}\
   .encountertime { color: " + classColorOne + "!important;} .maxhit { color: " + classColorTwo + "!important;}";
 }if (classColors == false && dpsGradient == false) {
